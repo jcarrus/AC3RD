9fe53797cefaef7a955617c1d68f29ab0294e0dc
Justin Carrus <carrus.justin@gmail.com>
2016-02-26 00:41:11 -0500

    Add SoftTimer library and update main to skeleton code

--- AC3RD-Arduino/AC3RD-Arduino.ino
+++ AC3RD-Arduino/AC3RD-Arduino.ino
@@ -1,8 +1,46 @@
+/* This project
+ *
+ *
+ *
+ */
+
+// Include library for timers
+#include "SoftTimer.h"
+
+// Include library for GPS
+
+Task mainTask(50, main);
+Task getCurrentGPSTask(1000, getCurrentGPS);
+
+double goalGPSLat;
+double goalGPSLon;
+double currGPSLat;
+double currGPSLon;
+
 void setup(){
   Serial.begin(9600);
+  SoftTimer.add(&mainTask);
 }
 
-void loop(){
-     // run things here
-  Serial.println("Hello world");
+// 
+void main(Task* me){
+  // Calculate the goal heading
+  double lonDiff = goalGPSLon - currGPSLon;
+
+  // Compare to possible headings and select best
+
+
+  // Calculate optimal tail and rudder angles
+
+  // Set tail and rudder angles
+
+}
+
+// Updates global variables 
+void getCurrentGPS(Task* me){
+
+  // Do stuff to talk to GPS module
+
+  currGPSLat = something;
+  currGPSLon = somethingelse;
 }
