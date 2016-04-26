66eef32029c7117cbd46ba404d20f76a9d16126a
Justin Carrus <carrus.justin@gmail.com>
2016-04-26 12:25:05 -0400

    Organizational changes

--- AC3RD-Arduino/AC3RD-Arduino.ino
+++ AC3RD-Arduino/AC3RD-Arduino.ino
@@ -1,184 +1,288 @@
-/* This project
- *
- *
- *
+/*
+ * This spring 2016 project for 2.017 by Ali Trueworthy, Brian
+ * Gilligan, Jorlyn LeGarrec, Justin Carrus, Trevor Day, and Val Peng.
  */
 
-// Include library for timers
+///////////////////
+// Timer Library //
+///////////////////
 #include "SoftTimer.h"
- //Library for Xbee Communication
+
+//////////
+// XBee //
+//////////
 #include "SoftwareSerial.h"
 SoftwareSerial Xbee(2,3); //RX TX
-// Include library for GPS
+void setupXBee(){
+	XBee.begin(9600);
+}
+
+/////////
+// GPS //
+/////////
 #include "Adafruit_GPS.h"
-// Include library for IMU
-//#include "LPS.h"
+int deadZone = (int) 45 * PI / 180;
+double goalGPSTolerance;
+double goalGPSLat;
+double goalGPSLon;
+int headingToGoal;
+double gpsLat; 
+double gpsLon;
+int gpsHeading;
+void getGPS(){
+  // Do stuff to talk to GPS module- even if the position stays the same, want the data
+  //parse data given in a NMEA string
+  GPS.parse(GPS.lastNMEA());
+  //get desired data
+  gpsLat     = GPS.latitude();
+  gpsLon     = GPS.longitude();
+  gpsHeading = GPS.angle();
+}
+void measure(double lat1, double lon1, double lat2, double lon2){
+	double R = 6378.137; // Radius of earth in KM
+	double dLat = (lat2 - lat1) * PI / 180;
+	double dLon = (lon2 - lon1) * PI / 180;
+	double a = pow(sin(dLat/2), 2) + cos(lat1 * PI / 180) * cos(lat2 * PI / 180) * pow(sin(dLon/2), 2);
+	double c = 2 * atan2(sqrt(a), sqrt(1-a));
+	distToGoal = R * c * 1000; // meters
+	double x = cos(lat2) * sin(dLon);
+	double y = cos(lat1)*sin(lat2) - sin(lat1)*cos(lat2)*cos(dLon);							
+	headingToGoal = (int) (atan2(x, y) / PI * 180); // degrees
+}
+
+
+/////////
+// IMU //
+/////////
 #include "LSM303.h"
-//#include "L3G.h"
 #include "wire.h"
-
-//Include arduino servo library
-#include "servo.h"
-
-//potentiometer library
-
-//Main task deals with tail and wing angle, variables: heading, tailDir, wingAngle
-Task mainTask(50, main);
-Task getCurrentGPSTask(1000, getCurrentGPS);
-Task getGoalGPS(1000, getGoalGPS);
-Task readEnvironmentTask(100, readEnvironment);
-
-//create servo objects
-Servo tailservo
-Servo rudderservo 
-//create variables
-double goalGPSLat=0; //sent from xbee
-double goalGPSLon=0; //sent from xbee
-double currGPSLat; 
-double currGPSLon;
-double currGPSangle;
-float heading; //from IMU angle with true north
-float headingDirect;
-float headingDesired;
-double windDir;
-double tailDir;
-double wingAngle; //relative to Front of the boat
-double rudderAngle;
-int potPinFront=3 //or whatever analog pin we use
-int potPinBack=4 //or whatever pin we use
-
-// put a glass over the wind sendor to calibrate it. adjust the zeroWindAdjustmet until sensor reads about zero
-//wind sensor calibration- need a 5V power source to wind sensor
-const float zeroWindAdjustment =  0; // negative numbers yield smaller wind speeds and vice versa.
-
-int TMP_Therm_ADunits;  //temp termistor value from wind sensor
-float RV_Wind_ADunits;    //RV output from wind sensor 
-float RV_Wind_Volts;
-unsigned long lastMillis;
-int TempCtimes100;
-float zeroWind_ADunits;
-float zeroWind_volts;
-float WindSpeed_MPH;
-
-void setup(Task*me){
-  Serial.begin(9600);
-  SoftTimer.add(&mainTask);
-  //Xbee
-  Xbee.begin(9600);
-  //servo
-  servo.attach(pin);
-  servo.attach(pin);
-  //IMU
-  wire.begin();
-  compass.init();
-  compass.enableDefault();
+LSM303 imu;
+float heading;
+void setupIMU(){	
+	wire.begin();
+  imu.init();
+  imu.enableDefault();
   /*
   Calibration values; the default values of +/-32767 for each axis
   lead to an assumed magnetometer bias of 0. Use the Calibrate example
   program to determine appropriate values for your particular unit.
   */
-  compass.m_min = (LSM303::vector<int16_t>){-32767, -32767, -32767};
-  compass.m_max = (LSM303::vector<int16_t>){+32767, +32767, +32767};
-
-  // GPS
-  GPS.begin(9600)
-
-  //include various configurations, need to set up the actual arduino
-  //include pinModes
+  imu.m_min = (LSM303::vector<int16_t>){-32767, -32767, -32767};
+  imu.m_max = (LSM303::vector<int16_t>){+32767, +32767, +32767};
+	SoftTimer.add(&taskIMU);
+}
+void taskIMU(Task* me){
+	heading=imu.read();
+	imu.read();
+	Xbee.print(imu.a.x);
+	Xbee.print(',');
+	Xbee.print(imu.a.y);
+	Xbee.print(',');
+	Xbee.print(imu.a.z);
+	Xbee.print('\n');
 }
 
-void getGoalGPS(Task*me){
+/////////////////
+// Wind Sensor //
+/////////////////
+// code from:
+// https://github.com/moderndevice/Wind_Sensor/blob/master/WindSensor/WindSensor.ino
+//
+// put a glass over the wind sendor to calibrate it.  adjust the
+// zeroWindAdjustmet until sensor reads about zero wind sensor
+// calibration- need a 5V power source to wind sensor
+float zeroWindAdjustment =  0;
+float temp_raw;
+float RV_Wind_ADunits;
+float RV_Wind_Volts;
+float TempC;
+float zeroWind_ADunits;
+float zeroWind_volts;
+float windSpeed;
+void setupWind(){
+}
+void getWind(){
+	temp_raw = (float) analogRead(WIND_TMP_PIN);
+	RV_Wind_ADunits = (float) analogRead(WIND_SPD_PIN);
+	RV_Wind_Volts = (RV_Wind_ADunits * 0.0048828125);
+	TempCtimes100 = ((0.005 * (temp_raw * temp_raw)) - (16.862 * temp_raw) + 9075.4) / 100;
+	zeroWind_ADunits = -0.0006*(temp_raw * temp_raw) + 1.0727 * temp_raw + 47.172;
+	zeroWind_volts = (zeroWind_ADunits * 0.0048828125) - zeroWindAdjustment;  
+	windSpeed =  pow(((RV_Wind_Volts - zeroWind_volts) /.2300) , 2.7265) * 0.44704; 
+} 
 
-  //Communicates with Xbee granted input "lat,lon"
-  while (Serial.available()>0)
-  goalGPSlon= Serial.parseInt();
-  goalGPSlat= Serial.parseInt();
- } 
-
-void getCurrentGPS(Task* me){
-
-  // Do stuff to talk to GPS module- even if the position stays the same, want the data
-  //parse data given in a NMEA string
-  GPS.parse(GPS.lastNMEA());
-  //get desired data
-  currGPSLat =GPS.latitude() ;
-  currGPSLon =GPS.longitude() ;
-  currGPSangle=GPS.angle();
+////////////
+// Servos //
+////////////
+#include "servo.h"
+#define TAIL_PIN 5
+#define RUDDER_PIN 6
+Servo tailServo;
+Servo rudderServo;
+int tailAngle;
+int rudderAngle;
+void setupServo(){
+	tailServo.attach(TAIL_PIN);
+  rudderServo.attach(RUDDER_PIN);
+}
+void setHeading(int h){
+	while (h < 0){
+		h += 360;
+	}
+	headingDesired = h % 360;
+}
+void setTailAngle(int a){
+	tailServo.write(a);
+	tailAngle = a;
+}
+void setRudderAngle(int a){
+	rudderServo.write(a);
+	rudderAngle = a;
 }
 
-void readEnvironment(){
-  //wind sensor- arduino code from https://github.com/moderndevice/Wind_Sensor/blob/master/WindSensor/WindSensor.ino 
-  //temp info might unnecessary
-      
-      TMP_Therm_ADunits = analogRead(analogPinForTMP);
-      RV_Wind_ADunits = analogRead(analogPinForRV);
-      RV_Wind_Volts = (RV_Wind_ADunits *  0.0048828125);
-
-      // these are all derived from regressions from raw data as such they depend on a lot of experimental factors
-      // such as accuracy of temp sensors, and voltage at the actual wind sensor, (wire losses) which were unaccouted for.
-      TempCtimes100 = (0.005 *((float)TMP_Therm_ADunits * (float)TMP_Therm_ADunits)) - (16.862 * (float)TMP_Therm_ADunits) + 9075.4;  
-
-      zeroWind_ADunits = -0.0006*((float)TMP_Therm_ADunits * (float)TMP_Therm_ADunits) + 1.0727 * (float)TMP_Therm_ADunits + 47.172;  //  13.0C  553  482.39
-
-      zeroWind_volts = (zeroWind_ADunits * 0.0048828125) - zeroWindAdjustment;  
-
-      // This from a regression from data in the form of 
-      // Vraw = V0 + b * WindSpeed ^ c
-      // V0 is zero wind at a particular temperature
-      // The constants b and c were determined by some Excel wrangling with the solver.
-    
-      WindSpeed_MPH =  pow(((RV_Wind_Volts - zeroWind_volts) /.2300) , 2.7265); 
- 
+////////////////////
+// Potentiometers //
+////////////////////
+#define POT_PIN_1 3
+#define POT_PIN_2 4
+int windDir; // any value between 0 and 359, points into the wind
+double wingAngle;
+int frontPotAngle;
+int rearPotAngle;
+void getWindAngle(){
+	frontPotAngle = map(analogread(POT_PIN_1), 0, 1023, 0, 330);
+	rearPotAngle  = map(analogread(POT_PIN_2), 0, 1023, 0, 330);
+  if (frontPotAngle >= 0 && frontPotAngle <= 200){
+		wingAngle = frontPotAngle - 90;
+	} else {
+		wingAngle = rearPotAngle + 90;
+	}
+  windDir = (heading + wingAngle + tailAngle) % 360; // Points into the wind
 }
 
 
-//Main task controls the motion of the boat
+///////////////////////
+// Task Declarations //
+///////////////////////
+Task fastTask(10, fast);
+Task mainTask(50, main);
+Task commTask(200, comm);
+
+///////////
+// Setup //
+///////////
+
+void setup(Task*me){
+	setupXBee();
+	setupIMU();
+	setupServo();
+	SoftTimer.add(&fastTask);
+	SoftTimer.add(&commTask);
+  SoftTimer.add(&mainTask);
+}
+
+float headingDirect;
+float headingDesired;
+
 void main(Task* me){
-  // Get heading from IMU
-  // heading is the angle of the IMU with true north
-  heading=compass.read();
-  // Read the tail angle, should begin at zero
-  tailAngle=tailservo.read();
-  //find wingAngle with reference to the front of the boat
-  //attach potentiometers such that 90 on front is front of boat and 90 on back is the back of the boat 
-  int frontPotAngle= (map(analogread(potPinFront),0, 1023, 0, 330);
-  int backPotAngle= (map(analogread(potPinBack), 0, 1023, 0, 330));
-  if (frontPotAngle>=0 && frontPotAngle<=180)
-    {wingAngle=frontPotAngle-90}
-  else if (backPotAngle>=0 && backPotAngle<=180)
-    {wingAngle=backPotAngle+90} 
-  //know wind direction
-  windDir=heading+wingAngle+tailAngle //this depends on us correctly orienting the servo 
+	// If we are at the destination, stop
+	if (distToGoal < goalGPSTolerance){
+		rudderServo.write(rudderServoMax);
+		tailServo.write(tailServoZero);
+		return;
+	}
 
-  // have the heading, wind direction and goal GPS location. Next need to know the direct heading
-  // Calculate the heading to Goal GPS location
-  double lonDiff = goalGPSLon - currGPSLon;
-  double latDiff = goalGPSLat - currGPSLat;
-  //convert this to an angle or we might be able to pull angles directly
-  double angleDiff=goalGPSangle-currGPSangle;
-  headingDirect=;
-  
-// we cannot go anywhere withing 45 degrees of the wind
-// if the desired, most direct heading is not within 45 degrees of the wind, want to go in the direction of desired heading 
-  if (headingDirect<=-windDir+45 && headingDesired>=-windDir-45)
-    {if (headingDirect-(-windDir+45)<=headingDirect-(windDir-45))
-      {headingDesired=headingDirect-(-wondDir+45)}
-      else {headingDesired=headingDirect-(-windDir-45)}
-    }
-  else {headingDirect=headingDesired};
+  // we cannot go anywhere withing the deadZone of the wind
+	// if the desired, most direct heading is not within 45 degrees of the wind,
+	// want to go in the direction of desired heading
 
+	
+	// If we want to sail in the deadZone
+	if (abs(headingToGoal - windDir) <= deadZone || abs(headingToGoal - windDir) >= 360 - deadZone){
+		// and were previously on a direct route
+		if (routeChoice == 0){
+			if ((abs(headingToGoal - windDir) < 180 && headingToGoal > windDir) ||
+					(abs(headingToGoal - windDir) > 180 && headingToGoal < windDir)){
+				setHeading(windDir + deadZone);
+				routeChoice = 1;
+			} else {
+				setHeading(windDir - deadZone);
+				routeChoice = -1;
+			}
+		} else {
+			if (routeChoice == 1){
+				setHeading(windDir + deadZone);
+			} else {
+				setHeading(windDir - deadZone);
+			}
+		}
+	} else {
+		routeChoice = 0;
+		setHeading(headingToGoal);
+	}
+	
   //generate a force in the forward direction
-  if (-windDir-heading>=0)
-      {tailAngle=5}
-    else {tailAngle=-5}
+  if ((abs(headingDesired - windDir) < 180 && headingDesired > windDir) ||
+			(abs(headingDesired - windDir) > 180 && headingDesired < windDir)){
+		setTailAngle(-liftTailAngle);
+	}	else {
+		setTailAngle( liftTailAngle);
+	}
+}
 
-  //give angle of attack, decide positive or negative 5 degrees
+void rudder(Task* me){
+	// Simple proportional controller with feedback
+	if ((abs(heading - headingDesired) < 180 && heading > headingDesired) ||
+   		 abs(heading - headingDesired) > 180 && heading < headingDesired)){
+		setRudderAngle(-kRudder * abs(heading - headingDesired));
+	} else {
+		setRudderAngle( kRudder * abs(heading - headingDesired));
+	}
+}
 
-  tailservo.write(tailAngle);
-  //choose rudder angle
-
-  //Set rudder angle
-  rudderservo.write(rudderAngle);
 
 
+char buf[8];
+void comm(Task* me){
+	if (XBee.available() >= 7){
+		for (int i = 0; i < 3; i++){
+			buf[i] = (char) XBee.read();
+		}
+		switch (buffer){
+		// gla: set goal latitude
+		case "setglat":
+			break;
+		case "getglat":
+			break;
+		// glo: set goal longitude
+		case "setglon":
+			break;
+		case "getglon":
+			break;
+		case "setdzne":
+		  break;
+		case "getdzne":
+			break;
+		case "setrcon":
+			break;
+		case "getrcon":
+			break;
+		case "setkrud":
+			kRudder = commReadInt();
+			break;
+		case "getkrud":
+			XBee.read();
+			XBee.println(kRudder);
+			break;
+		}
+	}
+}
 
+int commReadInt(){
+	String str = "";
+	while(XBee.peek() != '\n'){
+		str += XBee.read();
+	}
+	XBee.read();
+	return str.toInt();
+}
