fe6616cd58ca63dae61ba43d7e62ce7e77dcd7e9
Justin Carrus <carrus.justin@gmail.com>
2016-04-28 11:47:16 -0400

    Organizational changes

--- AC3RD-Arduino/AC3RD-Arduino.ino
+++ AC3RD-Arduino/AC3RD-Arduino.ino
@@ -4,26 +4,25 @@
  */
 
 //////////
-// XBee //
+// Comm //
 //////////
-#include <SoftwareSerial.h>
-SoftwareSerial Xbee(2,3); //RX TX
-void setupXBee(){
-	//XBee.begin(9600);
-  Serial.begin(9600);
+//#include <SoftwareSerial.h>
+//SoftwareSerial Serial(2,3); //RX TX
+void setupComm(){
+	Serial.begin(19200);
+  Serial.flush();
 }
-
 
 /////////
 // GPS //
 /////////
 
-#include "Adafruit_GPS.h"
+#include "./Adafruit_GPS.h"
 SoftwareSerial gpsSerial(3, 2);
 Adafruit_GPS GPS(&gpsSerial);
 #define PI 3.14159
 #define RADIUS_EARTH 6378.137
-int deadZone = (int) (45 * PI / 180);
+int deadZone = 25;
 double goalGPSTolerance;
 float goalGPSLat;
 float goalGPSLon;
@@ -55,10 +54,10 @@ int gpsAngleTo(double lat1, double lon1, double lat2, double lon2){
 /////////
 // IMU //
 /////////
-#include "LSM303.h"
+#include "./LSM303.h"
 #include "Wire.h"
 LSM303 imu;
-float heading;
+int heading;
 void setupIMU(){	
 	Wire.begin();
   imu.init();
@@ -68,21 +67,12 @@ void setupIMU(){
   lead to an assumed magnetometer bias of 0. Use the Calibrate example
   program to determine appropriate values for your particular unit.
   */
-  imu.m_min = (LSM303::vector<int16_t>){-32767, -32767, -32767};
-  imu.m_max = (LSM303::vector<int16_t>){+32767, +32767, +32767};
+  imu.m_min = (LSM303::vector<int16_t>){-2336, -2481, -2576};
+  imu.m_max = (LSM303::vector<int16_t>){+2694, +2241, +2484};
 }
-void readIMU(){
+void readHeading(){
 	imu.read();
-	heading = imu.heading((LSM303::vector<int>){1, 0, 0});
-	Serial.println(heading);
-	/*
-	Xbee.print(imu.a.x);
-	Xbee.print(',');
-	Xbee.print(imu.a.y);
-	Xbee.print(',');
-	Xbee.print(imu.a.z);
-	Xbee.print('\n');
-	*/
+	heading = imu.heading((LSM303::vector<int>){-1, 0, 0});
 }
 
 /////////////////
@@ -120,190 +110,278 @@ void getWind(){
 // Servos //
 ////////////
 #include <Servo.h>
-#define TAIL_PIN 5
-#define RUDDER_PIN 6
+#define TAIL_PIN 9
+#define RUDDER_PIN 10
 Servo tailServo;
 Servo rudderServo;
-int tailAngle;
-int rudderAngle;
-int kRudder; 
-int rudderControl;
-int headingDesired;
+int rudderServoOffset = 95;
+int rudderServoDir = 1;
+int tailServoOffset = 75;
+int tailServoDir = -1;
 void setupServo(){
 	tailServo.attach(TAIL_PIN);
   rudderServo.attach(RUDDER_PIN);
 }
-void setHeading(int h){
-	while (h < 0){
-		h += 360;
-	}
-	headingDesired = h % 360;
+void zeroServos(){
+  tailServo.detach();
+  rudderServo.detach();
+  delay(5000);
+  setupServo();
 }
-void setTailAngle(int a){
-	tailServo.write(a);
-	tailAngle = a;
+void tailServoFlip(){
+  tailServoDir *= -1;
 }
-void setRudderAngle(int a){
-	rudderServo.write(a);
-	rudderAngle = a;
+void rudderServoFlip(){
+  rudderServoDir *= -1;
 }
 
 ////////////////////
 // Potentiometers //
 ////////////////////
-#define POT_PIN_1 3
-#define POT_PIN_2 4
+#define POT_PIN_1 1
+#define POT_PIN_2 2
 int windDir; // any value between 0 and 359, points into the wind
-double wingAngle;
+int wingAngle;
 int frontPotAngle;
 int rearPotAngle;
-void getWindAngle(){
+void readWingAngle(){
 	frontPotAngle = map(analogRead(POT_PIN_1), 0, 1023, 0, 330);
 	rearPotAngle  = map(analogRead(POT_PIN_2), 0, 1023, 0, 330);
-  if (frontPotAngle >= 0 && frontPotAngle <= 200){
-		wingAngle = frontPotAngle - 90;
+  if (true || frontPotAngle >= 90 && frontPotAngle <= 270){
+		wingAngle = ((frontPotAngle - 120) + 360) % 360;
 	} else {
-		wingAngle = rearPotAngle + 90;
+		wingAngle = ((rearPotAngle + 60) + 360) % 360;
 	}
+}
+
+
+/////////////////////
+// State Variables //
+/////////////////////
+int rudderAngle;
+int tailAngle;
+
+
+///////////
+// Sense //
+///////////
+
+void sense(){
+  readWingAngle();
+  readHeading();
   windDir = (heading + wingAngle + tailAngle) % 360; // Points into the wind
 }
 
-///////////
-// Setup //
-///////////
+//////////////
+// Navigate //
+//////////////
 
-void setup(){
-	setupXBee();
-	setupIMU();
-	setupServo();
+int distToGoal = 999999;
+int headingDesired;
+int tailServoMax = 90;
+int tailServoZero;
+int liftTailAngle = 30;
+int routeChoice = 0;
+int navigationMode = 1;
+void setHeading(int h){
+  while (h < 0){
+    h += 360;
+  }
+  headingDesired = h % 360;
 }
-
-float headingDirect;
-float headingDesired;
-
-void loop(){
-
-  // sense reads sensors and updates values
-  //sense();
-	readIMU();
-  
-  // navigate sets a desired heading
-  //navigate();
-
-  // tail sets manages the tail angle for forward motion
-  //tail();
-
-  // rudder manages the rudder angles for the desired heading
-  //rudder();
-
-	// comm checks the communication channels for commands
-  //comm();
-
+int suggestHeading(int h){
+  // If we want to sail in the deadZone
+  if (abs(h - windDir) <= deadZone || abs(h - windDir) >= 360 - deadZone){
+    // and were previously on a direct route
+    if (routeChoice == 0){
+      if ((abs(h - windDir) < 180 && h > windDir) || (abs(h - windDir) > 180 && h < windDir)){
+        routeChoice = 1;
+        return windDir + deadZone;
+      } else {
+        routeChoice = -1;
+        return windDir - deadZone;
+      }
+    } else {
+      if (routeChoice == 1){
+        return windDir + deadZone;
+      } else {
+        return windDir - deadZone;
+      }
+    }
+  } else {
+    routeChoice = 0;
+    return h;
+  }
 }
-
-void sense(){
-}
-
 void navigate(){
-  gpsDistanceTo(gpsLat, gpsLon, goalGPSLat, goalGPSLon);
-	// If we are at the destination, stop
-	if (distToGoal < goalGPSTolerance){
-		rudderServo.write(rudderServoMax);
-		tailServo.write(tailServoZero);
-		return;
-	}
+  // Mode 0 maintains a straight line
+  switch (navigationMode) {
+    // 0 - Passive Sailing
+    case 0:
+      setHeading(heading);
+      break;
+    // 1 - Maintain a heading (managed by communicate
+    case 1:
+      setHeading(suggestHeading(headingToGoal));
+      break;
+    case 2:
+      gpsDistanceTo(gpsLat, gpsLon, goalGPSLat, goalGPSLon);
+      // If we are at the destination, stop
+      if (distToGoal < goalGPSTolerance){
+      }
+    default:
+      Serial.println("901");
+    break;
+  }
+}
 
-  // we cannot go anywhere withing the deadZone of the wind
-	// if the desired, most direct heading is not within 45 degrees of the wind,
-	// want to go in the direction of desired heading
+//////////
+// Sail //
+//////////
 
-	
-	// If we want to sail in the deadZone
-	if (abs(headingToGoal - windDir) <= deadZone || abs(headingToGoal - windDir) >= 360 - deadZone){
-		// and were previously on a direct route
-		if (routeChoice == 0){
-			if ((abs(headingToGoal - windDir) < 180 && headingToGoal > windDir) ||
-					(abs(headingToGoal - windDir) > 180 && headingToGoal < windDir)){
-				setHeading(windDir + deadZone);
-				routeChoice = 1;
-			} else {
-				setHeading(windDir - deadZone);
-				routeChoice = -1;
-			}
-		} else {
-			if (routeChoice == 1){
-				setHeading(windDir + deadZone);
-			} else {
-				setHeading(windDir - deadZone);
-			}
-		}
-	} else {
-		routeChoice = 0;
-		setHeading(headingToGoal);
-	}
-	
+void setTailAngle(int a){
+  tailServo.write(tailServoOffset + tailServoDir*a);
+  tailAngle = a;
+}
+void sail(){
   //generate a force in the forward direction
   if ((abs(headingDesired - windDir) < 180 && headingDesired > windDir) ||
-			(abs(headingDesired - windDir) > 180 && headingDesired < windDir)){
-		setTailAngle(-liftTailAngle);
-	}	else {
-		setTailAngle( liftTailAngle);
-	}
-}
-
-void tail(){
-
-}
-
-void rudder(){
-  if (rudderControl==1){
-	 // Simple proportional controller with feedback
-	 if ((abs(heading - headingDesired) < 180 && heading > headingDesired) ||
-   		abs(heading - headingDesired) > 180 && heading < headingDesired)){
-		  setRudderAngle(-kRudder * abs(heading - headingDesired));
-	 }  
-    else {
-		  setRudderAngle( kRudder * abs(heading - headingDesired));
-	 }
-  }
-
-
-  if (rudderControl==2){
-    //PD controller
-    if ((abs(heading - headingDesired) < 180 && heading > headingDesired) ||
-         abs(heading - headingDesired) > 180 && heading < headingDesired)){
-      setRudderAngle(-kRudder * abs(heading - headingDesired)-); // Needs more stuff
-    }  
-    else {
-      setRudderAngle( kRudder * abs(heading - headingDesired)-); // Needs more stuff
-    } 
+      (abs(headingDesired - windDir) > 180 && headingDesired < windDir)){
+    setTailAngle(-liftTailAngle);
+  } else {
+    setTailAngle( liftTailAngle);
   }
 }
 
-char buf[8];
-void comm(){
-	if (XBee.available() >= 7){
-		for (int i = 0; i < 3; i++){
-			buf[i] = (char) XBee.read();
-		}
-		switch (buffer){
+//////////
+// Turn //
+//////////
+
+int rudderControl = 1;
+int rudderServoMax;
+int rudderServoZero;
+int kRudder = 5;
+int maxRudderAngle = 45;
+void setRudderAngle(int a){
+  Serial.println(a);
+  a = min(a, maxRudderAngle);
+  a = max(a, -maxRudderAngle);
+  rudderServo.write(rudderServoOffset + rudderServoDir*a);
+  rudderAngle = a;
+}
+void turn(){
+  switch (rudderControl){
+    case 1:
+	    // Simple proportional controller with feedback
+	    if (((abs(heading - headingDesired) < 180) && (heading > headingDesired)) ||
+   		    ((abs(heading - headingDesired) > 180) && (heading < headingDesired))){
+		    setRudderAngle(-kRudder * abs(heading - headingDesired));
+	    }  
+      else {
+		    setRudderAngle( kRudder * abs(heading - headingDesired));
+	    }
+     break;
+    case 2:
+      //PD controller
+      if (((abs(heading - headingDesired) < 180) && (heading > headingDesired)) ||
+         ((abs(heading - headingDesired) > 180) && (heading < headingDesired))){
+        setRudderAngle(-kRudder * abs(heading - headingDesired)-0); // Needs more stuff
+      } else {
+        setRudderAngle( kRudder * abs(heading - headingDesired)-0); // Needs more stuff
+      }
+      break;
+    default:
+      Serial.println("902");
+    break;
+  }
+}
+
+/////////////////
+// Communicate //
+/////////////////
+
+String str;
+int commCode;
+int commReadInt(){
+  str = "";
+  while((char) Serial.peek() != '\n'){
+    str += (char) Serial.read();
+  }
+  Serial.read();
+  Serial.println(str);
+  return str.toInt();
+}
+void communicate(){
+  if (Serial.available()){
+    str = "";
+    while ((char) Serial.peek() != ','){
+      if ((char) Serial.peek() == '\n'){
+        Serial.read();
+        return;
+      }
+      str += (char) Serial.read();
+    }
+    Serial.read();
+    commCode = str.toInt();
+    switch (commCode){
+      //  0 -  9 are navigation commands
+      case 1:
+        headingToGoal = commReadInt();
+        break;
+
+      // 10 - 30 are configuration commands
+      case 10:
+        zeroServos();
+        Serial.read();
+        break;
+      case 11:
+        tailServoFlip();
+        Serial.read();
+        break;
+      case 12:
+        rudderServoFlip();
+        Serial.read();
+        break;
+      case 13:
+        setTailAngle(commReadInt());
+        delay(5000);
+        break;
+      case 14:
+        setRudderAngle(commReadInt());
+        delay(5000);
+        break;
+      case 15:
+        deadZone = commReadInt();
+        break;
+      case 16:
+        liftTailAngle = commReadInt();
+        break;
+      case 17:
+        maxRudderAngle = commReadInt();
+        break;
+      default:
+        Serial.println("902");
+      break;
+    }
+
+    /*
 		// setglat: set goal latitude
 		case "setglat":
       goalGPSLat = commReadInt();
 			break;
     //getglat: get goal latitude
 		case "getglat":
-      Xbee.read();
-      Xbee.println(goalGPSLat);
+      Serial.read();
+      Serial.println(goalGPSLat);
 			break;
 		// setglon: set goal longitude
 		case "setglon":
       goalGPSLon = commReadInt();
 			break;
-    //getglon: gets goal GPS Longitude from Xbee
+    //getglon: gets goal GPS Longitude from Serial
 		case "getglon":
-      Xbee.read();
-      Xbee.println(goalGPSLon);
+      Serial.read();
+      Serial.println(goalGPSLon);
 			break;
     //setdzne: sets the value of the deadzone
 		case "setdzne":
@@ -311,8 +389,8 @@ void comm(){
 		  break;
     //gets the value of the deadzone
 		case "getdzne":
-      XBee.read();
-      XBee.println(deadZone);
+      Serial.read();
+      Serial.println(deadZone);
 			break;
     //setrcon: sets the type of rudder control we are using
 		case "setrcon":
@@ -324,10 +402,10 @@ void comm(){
 		case "setkrud":
 			kRudder = commReadInt();
 			break;
-    //getkrud: gets the rudder gain from Xbee
+    //getkrud: gets the rudder gain from Serial
 		case "getkrud":
-			XBee.read();
-			XBee.println(kRudder);
+			Serial.read();
+			Serial.println(kRudder);
 			break;
     // nvmd: navigation mode
     //       1 - GPS navigation
@@ -336,16 +414,52 @@ void comm(){
     case "getgnav":
 
       break;
-
-		}
-	}
+*/
+  }
+  Serial.print(millis());
+  Serial.print(',');
+  Serial.print(tailAngle);
+  Serial.print(',');
+  Serial.print(rudderAngle);
+  Serial.print(',');
+  Serial.print(heading);
+  Serial.print(',');
+  Serial.print(headingDesired);
+  Serial.print(',');
+  Serial.print(wingAngle);
+  Serial.print(',');
+  Serial.print(windDir);
+  Serial.print(',');
+  Serial.print(routeChoice);
+  Serial.print(',');
+  Serial.print(imu.a.x);
+  Serial.print(',');
+  Serial.print(imu.a.y);
+  Serial.print(',');
+  Serial.print(imu.a.z);
+  Serial.print(',');
+  Serial.print(imu.m.x);
+  Serial.print(',');
+  Serial.print(imu.m.y);
+  Serial.print(',');
+  Serial.print(imu.m.z);
+  Serial.print('\n');
 }
 
-int commReadInt(){
-	String str = "";
-	while(XBee.peek() != '\n'){
-		str += XBee.read();
-	}
-	XBee.read();
-	return str.toInt();
+////////////////////
+// Setup and Loop //
+////////////////////
+
+void setup(){
+  setupComm();
+  setupIMU();
+  setupServo();
+}
+
+void loop(){
+  sense();
+  navigate();
+  sail();
+  turn();
+  communicate();
 }
